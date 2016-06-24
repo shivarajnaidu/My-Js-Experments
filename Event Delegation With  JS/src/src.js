64029c8c1dff37e910c66f3d52dabc9f58786f8d
shivarajnaidu <shivarajnaidu@gmail.com>
2016-06-24 12:12:40 +0530

    small edits

--- Event Delegation With  JS/src/src.js
+++ Event Delegation With  JS/src/src.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
   "use strict";
+
   let Click_Event_Handler = function (event) {
+
     let t = event.target;
+
     this.style.backgroundColor = "#efefef";
+    
     if (t.hasAttribute("data-url") && t.nodeName == "P") {
-	  t.className = "cActivePara";
+	    t.className = "cActivePara";
       location.href=t.getAttribute("data-url");
     } else if (t.nodeName=="LI"){
-        t.className = "cActivelist";
+        t.className = "cActivelist";        
         t.innerHTML = "You have clicked me";
     } else {
         return false;
-    };
+    }
   };
-  document.querySelector("#uv").addEventListener("click", Click_Event_Handler );
+  
+  document.querySelector("#uv").addEventListener("click", Click_Event_Handler);
 });
